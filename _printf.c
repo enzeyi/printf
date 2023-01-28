1b1283dc314a1fbe63a145060cfe656f199e4735
Emmanuel Enzeyi <110012571+enzeyi@users.noreply.github.com>
2023-01-28 14:18:06 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,44 +1,39 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
 
-/**
- * _printf - printf function
- * @format: const char pointer
- * Return: b_len
- */
-int _printf(const char *format, ...)
-{
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+int _printf(const char *format, ...) {
+    va_list args;
+    int count = 0;
 
-	register int count = 0;
+    va_start(args, format);
 
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
-	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+    for (int i = 0; format[i] != '\0'; i++) {
+        if (format[i] == '%') {
+            i++;
+            switch(format[i]) {
+                case 'c':
+                    putchar(va_arg(args, int));
+                    count++;
+                    break;
+                case 's':
+                    fputs(va_arg(args, char*), stdout);
+                    count += strlen(va_arg(args, char*));
+                    break;
+                case '%':
+                    putchar('%');
+                    count++;
+                    break;
+                default:
+                    putchar(format[i]);
+                    count++;
+                    break;
+            }
+        } else {
+            putchar(format[i]);
+            count++;
+        }
+    }
+
+    va_end(args);
+    return count;
 }
